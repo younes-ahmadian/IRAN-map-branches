3841cb7d4e0f589bc1563d0c5756f653a3e61ebf
younes <younes_ahmadian@yahoo.com>
2022-06-29 12:54:51 -0700

    simple view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const branchIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img  className = "iranMap" src="../picture/IRAN.png" alt="IRAN MAP!"/>
+
+          { branchIndex.map(i => <a  href="https://www.google.com" target="_blank">
+                                    <div className={`points p${i}`}>P
+                                      <span className='tooltipText'>
+                                        شماره تلفن: <span className='text'>123456</span><br/>
+                                        آدرس: <span className='text'>آدرس شعبه اینجا وارد شود</span><br/>
+                                        مدیر شعبه: <span className='text'>نام مدیریت شعبه</span>
+                                      </span>
+                                    </div>
+                                 </a>
+                            )
+          }
+        
     </div>
   );
 }
